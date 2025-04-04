c761c7acf67a5d901079e2781533a8fbe117daff
Tarasstets <tarasstets0@gmail.com>
2025-04-04 12:54:16 +0300

    edit work

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,9 @@ export function displayTasks(tasks) {
         <button class="todo__edit-btn" data-index="${index}">
         <img src="./src/img/pencil.svg" alt="Delete" class="todo__edit-icon"
         </button>
+        <button class="todo__done-btn" data-index="${index}">
+        <img src="./src/img/done.svg" alt="Delete" class="todo__done-icon"
+        </button>
       </div>
     `;
     list.appendChild(li);
@@ -63,5 +66,57 @@ document.addEventListener("click", (event) => {
     tasks.splice(index, 1); // Видаляємо завдання з масиву
     setTasksInLocalStorage(tasks); // Оновлюємо LocalStorage
     displayTasks(tasks); // Оновлюємо список на сторінці
+  }
+
+  // DONE
+  if (event.target.classList.contains("todo__done-icon")) {
+    const taskItem = event.target.closest(".todo__item");
+    const taskText = taskItem.querySelector(".task-text");
+    const taskDifficulty = taskItem.querySelector(".difficulty");
+
+    taskText.classList.toggle("task-done");
+    taskDifficulty.classList.toggle("task-done");
+  }
+});
+
+const modal = document.getElementById("edit-modal");
+const modalInput = document.getElementById("edit-task-input");
+const modalSelect = document.getElementById("edit-difficulty");
+const modalSave = document.getElementById("modal-save");
+const modalClose = document.getElementById("modal-close");
+
+document.addEventListener("click", (event) => {
+  // Відкриття модального вікна
+  if (event.target.closest(".todo__edit-btn")) {
+    const btn = event.target.closest(".todo__edit-btn");
+    editingIndex = parseInt(btn.getAttribute("data-index"));
+    const tasks = getTasksFromLocalStorage();
+    const task = tasks[editingIndex];
+
+    modalInput.value = task.taskInput;
+    modalSelect.value = task.difficulty;
+
+    modal.style.display = "flex";
+  }
+
+  // Закриття
+  if (event.target === modal || event.target === modalClose) {
+    modal.style.display = "none";
+  }
+});
+
+// Збереження змін
+modalSave.addEventListener("click", () => {
+  const tasks = getTasksFromLocalStorage();
+
+  if (editingIndex !== null && tasks[editingIndex]) {
+    tasks[editingIndex].taskInput = modalInput.value;
+    tasks[editingIndex].difficulty = modalSelect.value;
+
+    setTasksInLocalStorage(tasks);
+    displayTasks(tasks);
+
+    modal.style.display = "none";
+    editingIndex = null;
   }
 });
