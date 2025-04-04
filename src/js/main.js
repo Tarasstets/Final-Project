b133a063b23a810f6cc97ccca9bb254e29f68689
Tarasstets <tarasstets0@gmail.com>
2025-04-04 23:51:40 +0300

    add slogan

--- src/js/main.js
+++ src/js/main.js
@@ -209,3 +209,19 @@ function updateClock() {
 }
 
 setInterval(updateClock, 1000);
+
+const slogans = [
+  "HAVE A NICE DAY",
+  "MAKE IT HAPPEN",
+  "YOU GOT THIS!",
+  "STAY PRODUCTIVE",
+  "KEEP MOVING FORWARD",
+  "ONE TASK AT A TIME",
+  "TODAY IS YOUR DAY",
+  "FOCUS & FINISH",
+  "BE YOUR BEST SELF",
+];
+
+const sloganElement = document.querySelector(".page__slogan-text");
+const randomIndex = Math.floor(Math.random() * slogans.length);
+sloganElement.textContent = slogans[randomIndex];
